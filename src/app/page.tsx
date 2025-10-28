5aa9e31063207c48a4fc820828d2948d9d56e3bb
Vishwa Lingam <vishwalingam24816@gmail.com>
2025-10-28 09:12:04 +0000

    now 2nd image is https://lh3.googleusercontent.com/d/1AjcnFRfWD0kNO7Ind2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ const storyImages = [
     hint: 'frustrated trader'
   },
   {
-    src: 'https://lh3.googleusercontent.com/d/1CBc6XF134NCQ0INI0PBSogSbqi4wDKXa',
+    src: 'https://lh3.googleusercontent.com/d/1AjcnFRfWD0kNO7Ind2UleKQAzGOh-9Gw',
     alt: 'A simplified, unified trading dashboard on a laptop.',
     hint: 'clean dashboard'
   },
